931f84e562c168ad6ccfe162ec7d157544b6eb74
xujiadong <piduoxu@qq.com>
2024-07-14 21:36:27 +0100

    d

--- tactile_feature_extraction/pytorch_models/supervised/train_model_w_metrics.py
+++ tactile_feature_extraction/pytorch_models/supervised/train_model_w_metrics.py
@@ -97,11 +97,11 @@ def train_model_w_metrics(
             if training:
                 optimizer.zero_grad()
                 if model_type == 'seq2seq_gru':
-                    outputs_tmp = model(inputs, output_last=False)
+                    labels = labels.permute(0, 2, 1)  # [batch_size, timesteps, out_dim]
+                    outputs_tmp = model(inputs, output_last=False, target=labels)
                     # 合并batch_size和timesteps
                     outputs = outputs_tmp.view(-1, outputs_tmp.size(-1))
                     # labels shape [batch_size, out_dim,timesteps]
-                    labels= labels.permute(0,2,1) # [batch_size, timesteps, out_dim]
                     labels = labels.contiguous().view(-1, labels.size(-1))
                     # labels = labels.view(-1, labels.size(-1))
                 else:
