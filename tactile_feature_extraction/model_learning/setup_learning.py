4f52b7138f8d94fb2585c67f07acb2c52375a0d8
xujiadong <piduoxu@qq.com>
2024-07-22 16:30:57 +0100

    d

--- tactile_feature_extraction/model_learning/setup_learning.py
+++ tactile_feature_extraction/model_learning/setup_learning.py
@@ -45,7 +45,7 @@ def setup_learning(save_dir=None):
     learning_params = {
         'seed': 42,
         'batch_size': 32,
-        'epochs': 10, # 100
+        'epochs': 30, # 100
         'lr': 1e-5, # 1e-4
         'lr_factor': 0.5,
         'lr_patience': 10,
