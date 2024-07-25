29fc0a95ad1d169dd3a06bcae2ea4b3fd13d2986
xujiadong <piduoxu@qq.com>
2024-07-25 10:39:06 +0100

    d

--- tactile_feature_extraction/pytorch_models/supervised/models.py
+++ tactile_feature_extraction/pytorch_models/supervised/models.py
@@ -157,7 +157,7 @@ def create_model(
             in_dim=in_dim,
             out_dim=out_dim,
             hidden_dim=128,
-            kernel_size=(3, 3),
+            kernel_size=(7, 5),
             num_layers=2,
 
         ).to(device)
